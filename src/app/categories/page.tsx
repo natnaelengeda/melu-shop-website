17cb423f790e2288dbe22c9082a08e136f1fa372
nattyengeda <nattynengeda@gmail.com>
2025-07-25 12:54:38 +0300

    Finish Home Page

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/layouts/default-layout'
+
+export default function page() {
+  return (
+    <DefaultLayout>
+      <div className=''>
+      </div>
+    </DefaultLayout>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,18 @@ import Image from "next/image"
 
 // AppAsset
 import AppAsset from "@/core/AppAsset"
+import Link from "next/link"
 
 export default function Header() {
+  const headerList = [
+    { name: "Categories", page: "/categories" },
+    { name: "Products", page: "/products" }
+  ]
   return (
     <header className="w-full h-20 bg-[#fcefe3]">
       {/* Main Content */}
-      <div className="w-full h-full px-5 md:px-10 mx-auto xl:container flex flex-row items-center justify-between">
+      <div
+        className="w-full h-full px-5 md:px-10 mx-auto xl:container flex flex-row items-center justify-between">
         <div className="flex flex-row items-center justify-start gap-2">
           <Image
             src={AppAsset.Logo}
@@ -17,7 +23,24 @@ export default function Header() {
           />
           <p className="font-cinzel text-xl text-black ">Melu Clothes Shop</p>
         </div>
+
+        {/* Nav Bar */}
+        <div className="w-full h-full flex items-center justify-end gap-5">
+          {
+            headerList.map((header, index) => {
+              return (
+                <Link
+                  href={header.page}
+                  className="cursor-pointer text-lg hover:text-xl transition-all duration-75">
+                  {header.name}
+                </Link>
+              )
+            })
+          }
+        </div>
       </div>
+
+
     </header>
   )
 }
